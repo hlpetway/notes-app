b8d6c2f39795161fea23c2e07f55f433a8f6dcb4
Hailey Petway <petwayh@gmail.com>
2014-05-14 17:45:18 -0700

    Added all routes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -14,5 +14,70 @@ module.exports.collection = function(req, res) {
         res.send(users);
         //if no errors sends the users array to the response.
     });
+};
 
+exports.findById = function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    Note.findOne({
+        "_id": req.params.id
+    }, function(err, user) {
+        if (err) {
+            res.send(500, {
+                error: err
+            });
+            return false;
+        }
+        res.send(user);
+    });
+};
+
+exports.create = function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var note = new Note(req.body);
+    note.save(function(err, resUser) {
+        if (err) {
+            res.send(500, {
+                error: err
+            });
+            return false;
+        }
+        res.send(resUser);
+    });
+};
+
+exports.update = function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var id = req.params.id;
+    delete req.body._id;
+
+    Note.update({
+        '_id': id
+    }, req.body, function(err) {
+        if (err) {
+            res.send(500, {
+                error: err
+            });
+            return false;
+        }
+        res.send({
+            "message": "success!"
+        });
+    })
+};
+
+exports.destroy = function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    Note.remove({
+        '_id': req.params.id
+    }, function(err) {
+        if (err) {
+            res.send(500, {
+                error: err
+            });
+            return false;
+        }
+        res.send({
+            "message": "success!"
+        });
+    });
 };
--- server.js
+++ server.js
@@ -3,21 +3,28 @@
 var express = require('express');
 var http = require('http');
 var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+
 var noteRoutes = require('./routes/noteRoutes');
-
-var app = express();
-
-//get all the notes
-app.get('/api/v0_0_1/notes', noteRoutes.collection);
 
 //connect to mongo server using mongoose
 
 mongoose.connect('mongodb://localhost/notes-development');
 
+var app = express();
+
+app.use(bodyparser());
+
+//all our methods we can perform with body-parser
+app.get('/api/v0_0_1/notes', noteRoutes.collection);
+app.get('/api/v0_0_1/note/:id', noteRoutes.findById);
+app.post('api/v0_0_1/notes', noteRoutes.create);
+app.put('api/v0_0_1/note/:id', noteRoutes.update);
+app.delete('api/v0_0_1/note/:id', noteRoutes.destroy);
+
 app.set('port', process.env.PORT || 3000);
 
 var server = http.createServer(app);
-
 server.listen(app.get('port'), function() {
     console.log('server listening on port' + app.get('port'));
 });
