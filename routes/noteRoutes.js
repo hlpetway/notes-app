0ce5304c30fe93c38d7fa119b12ea7a59547e101
Hailey Petway <petwayh@gmail.com>
2014-05-19 18:21:39 -0700

    working on debugging so database docs show

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -3,14 +3,14 @@ var Note = require('../models/note');
 module.exports.collection = function(req, res) {
     res.setHeader('Content-Type', 'application/json');
 
-    Note.find({}, function(err, users) {
+    Note.find({}, function(err, notes) {
         if (err) {
             res.send(500, {
                 err: err
             });
             return false;
         }
-        res.send(users);
+        res.send(notes);
 
     });
 };
